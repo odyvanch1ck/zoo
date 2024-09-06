061938d8af20fb6b0f01cc54625c79a4d0d535c4
unknown <mkovalev331@gmail.com>
2024-09-06 13:20:35 +0300

    test

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php
+?>
+<!-- html делает Данил -->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Мадагаскар</title>
+</head>
+<body>
+<!-- start header-->
+<header>
+    <a href="index.php" class="logo">
+        Мадагаскар</a>
+    <nav>
+        <ul>
+            <li><a href="#slider">Предложения и акции</a></li>
+            <li><a href="#employees">Сотрудники</a></li>
+            <li><a href="#animals">Животные</a></li>
+            <li><a href="#feedbacks">Отзывы посетителей</a></li>
+        </ul>
+    </nav>
+    <a href="tel:+79800571151">Позвоните нам!</a>
+</header>
+<!--end header-->
+<!--start main-->
+<main>
+    <!--start slider section-->
+    <section id="slider">
+        <div class="slider">
+            <div class="slide">
+                <h2>Мадагаскар</h2>
+                <p>Приходите пожалуйста, умоляю</p>
+            </div>
+            <div class="slide">
+                <h2>Мадагаскар</h2>
+                <p>Можете не приходить, мы вас не заставляем</p>
+            </div>
+            <div class="slide">
+                <h2>Мадагаскар</h2>
+                <p>Но с другой стороны у нас есть семья, поэтому придите пожалуйста</p>
+            </div>
+            <div class="slide">
+                <h2>Мадагаскар</h2>
+                <p>Приходите пожалуйста, умоляю</p>
+            </div>
+        </div>
+    </section>
+    <!--end slider section-->
+    <!--start card employee section-->
+    <section id="employees" class="employees">
+        <div>
+            <img src="https://i.vuzopedia.ru/storage/app/uploads/public/660/6a2/3c5/6606a23c55ad7726564881.jpg" alt="">
+            <p>Какой-то мужик</p>
+        </div>
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Baby_elephant_and_zoo_keeper_-Maryland_Zoo-8a.jpg/1200px-Baby_elephant_and_zoo_keeper_-Maryland_Zoo-8a.jpg"
+                 alt="">
+            <p>Какой-то мужик</p>
+        </div>
+        <div>
+            <img src="https://www.gazeta.uz/sp/14245786/img/tild3433-3333-4962-b134-356233383661___mg_1768.jpg" alt="">
+            <p>Какой-то мужик</p>
+        </div>
+            <div>
+                <img src="https://s9.stc.all.kpcdn.net/share/i/4/1365330/inx960x640.jpg" alt="">
+                <p>Какой-то мужик</p>
+            </div>
+            <div>
+                <img src="https://www.giffonifilmfestival.it/en/media/djcatalog2/images/zookeeper_f.jpg" alt="">
+                <p>Какой-то мужик</p>
+            </div>
+    </section>
+    <section id="animals">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </section>
+    <section id="feedbacks">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </section>
+    <!--start card employee section-->
+</main>
+<!--end main-->
+<footer>
+    <div></div>
+    <div></div>
+    <div></div>
+
+</footer>
+<script src="script.js"></script>
